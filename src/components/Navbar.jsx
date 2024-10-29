ffa62a372f41bec06d2b7f92456c478b6d34557e
22prerak22 <89489828+22prerak22@users.noreply.github.com>
2024-10-29 02:14:13 -0400

    Added Resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,18 @@ import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
-    <div className=" mb-20 flex items-center justify-between py-6">
+    <div className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
         <img src={logo} alt="logo" className="h-[36px] mx-2 w-11" />
+      </div>
+      <div className="btn">
+        <motion.a
+          href="/Prerak_Patel_Resume.docx"
+          download="Prerak_Patel_Resume.docx"
+          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300"
+        >
+          Download Resume
+        </motion.a>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <motion.a
@@ -33,7 +42,18 @@ const Navbar = () => {
         >
           <FaGithub />
         </motion.a>
-        <FaInstagram />
+        <motion.a
+          href="https://www.instagram.com/your-instagram-profile/" // Replace with your Instagram URL
+          target="_blank"
+          rel="noopener noreferrer"
+          whileHover={{
+            scale: 1.5,
+            rotate: 360,
+          }}
+          transition={{ duration: 0.5 }}
+        >
+          <FaInstagram />
+        </motion.a>
       </div>
     </div>
   );
